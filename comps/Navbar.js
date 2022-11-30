9bd2e9a3f9763ea2c8b3aab49597fc409297ae04
tekra-666 <shivinboxmail@gmail.com>
2022-11-30 15:49:03 +0530

    modified:   comps/Navbar.js modified:   pages/ninjas/[id].js

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <Image src="/logo.png" width={77} height={77}/>
+        <Image src="/logo.png" width={77} height={77} alt="logo"/>
       </div>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,38 +1,38 @@
 export const getStaticPaths = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = res.json();
+  const data = await res.json();
   
 
-  const paths = data.map (ninja => {
+  const paths = data.map(ninja => {
     return{
       params: { id: ninja.id.toString() }
     }
   })
 
   return {
-    paths: paths,
+    paths,
     fallback: false
   }
 }
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users' + id)
   const data = await res.json();
 
   return{
-    props: {ninjas: data}
+    props: {ninja: data}
   }
 }
 
-const Details = () => {
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>{ninja.name}</h1>
-      <h1>{ninja.email}</h1>
-      <h1>{ninja.website}</h1>
-      <h1>{ninja.address.city}</h1>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
     </div>
+
   );
 }
 
